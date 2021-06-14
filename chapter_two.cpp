2a1965bc4d2035f5b14f66d38c8d788dd014b725
Fabricio Dematte <fabriciodematte7p@gmail.com>
2021-06-14 17:41:34 -0300

    sol: partition

--- chapter_two.cpp
+++ chapter_two.cpp
@@ -59,10 +59,54 @@ int chapter_two_return_kth_to_last(LinkedList list, int kth) {
   return curr->data;
 }
 
+LinkedList chapter_two_delete_middle_node(Node *middle) {
+  // Delete middle node: Implement an algorithm to delete a node in the middle
+  // (i.e., any node but the first and last node, not necessarily the exact
+  // middle) of a singly linked list, given only access to that node.
+  //
+  // EXAMPLE
+  // Input: the node C from the linked list A -> B -> C -> D -> E -> F
+  // Output: A -> B -> D -> E -> F
+
+  Node *curr = middle;
+
+  while (curr->next->next != NULL) {
+    curr->data = curr->next->data;
+    curr = curr->next;
+  }
+
+  curr->data = curr->next->data;
+  curr->next = NULL;
+
+  LinkedList list = LinkedList({});
+  list.head = middle;
+
+  return list;
+}
+
+LinkedList chapter_two_partition(LinkedList list) {
+  // Partition: Write code to partition a liked list around a value x, such that
+  // all nodes less than x come before all nodes greater than or equal to x. If
+  // x is contained within the list, the values of x only need to be after the
+  // elements less than x (see below). The partition element x can appear
+  // anywhere in the "right partition"; it does not need to appear between the
+  // left and the right partitions.
+  //
+  // EXAMPLE
+  // Input: 3 -> 5 -> 8 -> 5 -> 10 -> 2 -> 1 [partition=5]
+  // Output: 3 -> 1 -> 2 -> 10 -> 5 -> 5 -> 8
+
+  return list;
+}
+
 void chapter_two() {
   LinkedList list = LinkedList({1, 2, 3, 1, 4, 5, 6, 1, 1, 5, 6, 9});
   LinkedList q1_expected = LinkedList({1, 2, 3, 4, 5, 6, 9});
   assert(chapter_two_remove_dups(list) == q1_expected);
 
   assert(chapter_two_return_kth_to_last(list, 3) == 5);
+
+  LinkedList list2 = LinkedList({1, 2, 3, 4, 5, 6});
+  LinkedList q2_expected = LinkedList({4, 5, 6});
+  assert(chapter_two_delete_middle_node(list2.head->next->next) == q2_expected);
 }
