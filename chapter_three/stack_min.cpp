8fcc07ecab1183304685a72e18389ac1750d54a8
Fabricio Dematte <fabriciodematte7p@gmail.com>
2021-06-18 20:49:39 -0300

    pre move to chapter_4 because i want to

---
+++ chapter_three/stack_min.cpp
@@ -0,0 +1,17 @@
+#include "../lib/stack.h"
+#include <bits/stdc++.h>
+using namespace std;
+//
+//
+//
+//   ___ _              _             ____
+//  / __| |_  __ _ _ __| |_ ___ _ _  |__ /
+// | (__| ' \/ _` | '_ \  _/ -_) '_|  |_ \
+//  \___|_||_\__,_| .__/\__\___|_|   |___/
+//                |_|
+//
+// Stack Min: How would you design a stack which, in addition to push and pop,
+// has a function `min()` which returns the minimum element? Push, pop and min
+// should all operate in O(1) time.
+
+int main() {}
